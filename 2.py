4902f6097f7cc7dc6b541fcf26ac0057d399b2ee
devyalin <devyalin@gmail.com>
2022-05-29 18:23:22 +0300

    init commit

---
+++ 2.py
@@ -0,0 +1,20 @@
+a = 7.3
+a = "Moshe"
+b = "David"
+i = {"name": "aviel"}
+c = a + " " + b
+d = f"{i['name']}  {b}"
+e = "%s e %s" %(a,b)
+f = "{} f {}".format(a, b)
+g = 'aviel'
+h = "aviel \"buskila\""
+j = 5
+k = a + str(j)
+
+print (c)
+print (d)
+print (e)
+print (f)
+print (h)
+print (i)
+print (k)
